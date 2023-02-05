bf7d7891ebc954b17b0a21ce36d0baa67b0566f3
Brian Ellis <bellis34@my.gcu.edu>
2023-02-04 19:04:48 -0700

    Add files via upload

---
+++ src/main/java/com/gcu/data/entity/OrderEntity.java
@@ -0,0 +1,112 @@
+package com.gcu.data.entity;
+
+//import org.springframework.data.annotation.Id;
+//import org.springframework.data.relational.core.mapping.Column;
+//import org.springframework.data.relational.core.mapping.Table;
+
+//@Table("orders")
+public class OrderEntity
+{
+	//@Id
+	Long id;
+	
+	//@Column("order_no")
+	String orderNo;
+	
+	//@Column("product_name")
+	String productName;
+	
+	//@Column("price")
+	float price;
+	
+	//@Column("quantity")
+	int quantity;
+
+	public OrderEntity() {
+		
+	}
+
+	public OrderEntity(Long id, String orderNo, String productName, float price, int quantity) {
+		this.id = id;
+		this.orderNo = orderNo;
+		this.productName = productName;
+		this.price = price;
+		this.quantity = quantity;
+	}
+
+	/**
+	 * @return the id
+	 */
+	public Long getId() {
+		return id;
+	}
+
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	/**
+	 * @return the orderNo
+	 */
+	public String getOrderNo() {
+		return orderNo;
+	}
+
+	/**
+	 * @param orderNo the orderNo to set
+	 */
+	public void setOrderNo(String orderNo) {
+		this.orderNo = orderNo;
+	}
+
+	/**
+	 * @return the productName
+	 */
+	public String getProductName() {
+		return productName;
+	}
+
+	/**
+	 * @param productName the productName to set
+	 */
+	public void setProductName(String productName) {
+		this.productName = productName;
+	}
+
+	/**
+	 * @return the price
+	 */
+	public float getPrice() {
+		return price;
+	}
+
+	/**
+	 * @param price the price to set
+	 */
+	public void setPrice(float price) {
+		this.price = price;
+	}
+
+	/**
+	 * @return the quantity
+	 */
+	public int getQuantity() {
+		return quantity;
+	}
+
+	/**
+	 * @param quantity the quantity to set
+	 */
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+	
+	
+	
+	
+	
+	
+}
